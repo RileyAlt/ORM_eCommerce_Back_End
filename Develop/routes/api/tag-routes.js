0546bad66c08dab9c76baf3498462f1c18dd25ec
RileyAlt <rileyaltenburg@gmail.com>
2023-03-29 12:42:02 -0500

    routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -20,9 +20,15 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findAll({
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }, { model: ProductTag }],
     });
+
+    if(!tagData) {
+      res.status(404).json({ message: ' No product associated with this ID'});
+      return;
+    }
+
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -31,6 +37,18 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagIdArr = await Tag.create({
+      tag_id: req.body.tag_id,
+    });
+    res.status(200).json(tagIdArr);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
   try {
     const tagData = await Tag.findAll({
       include: [{ model: Product }, { model: ProductTag }],
@@ -41,12 +59,24 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if(!tagData) {
+      res.status(404).json({ message: ' No product associated with this ID'});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
